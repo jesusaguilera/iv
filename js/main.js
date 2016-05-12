87bb095bfea00dd6fb4f85d15c037a8f6d03fbe5
jesus-aguilera-rh <jesus.aguilera@razorfishhealthware.com>
2016-05-12 22:37:22 +0200

    update

--- js/main.js
+++ js/main.js
@@ -925,6 +925,10 @@ angular.module('definicionesModule', [])
     {palabra:"Nesciencia", definicion:"Necedad, ignorancia. Falto de ciencia."},
     {palabra:"Manumisión", definicion:"En la antigua Roma proceso de liberar a un esclavo, tras lo cual se convertía en liberto."},
     {palabra:"Pormenorizar", definicion:"Describir o enumerar minuciosamente."},
+    {palabra:"Benemérito", definicion:"Digno de galardón."},
+    {palabra:"Albur", definicion:"Suerte o azar."},
+    {palabra:"Cuita", definicion:"Trabajo, aflicción, desventura, pena. / Ansia, anhelo, deseo vehemente."},
+    {palabra:"Pléyade", definicion:"Grupo de personas famosas, especialemnte en las letras, que viven en la misma época."},
   ];
 
 }]);
