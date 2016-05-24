8ff3c2d4f45cd2e58251eb4e2a78641fd5c7e835
jesus-aguilera-rh <jesus.aguilera@razorfishhealthware.com>
2016-05-24 12:18:57 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1001,6 +1001,7 @@ angular.module('definicionesModule', [])
     {palabra:"Recua", definicion:"Animales de carga."},
     {palabra:"Desuncir", definicion:"Liberar de yugo."},
     {palabra:"Explícito", definicion:"Que expresa clara y determinadamente una cosa."},
+    {palabra:"Torticero", definicion:"Injusto, o que no se arregla a las leyes o a la razón."},
   ];
 
 }]);
